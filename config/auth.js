6f9fb95caa98cbe4b8fa5dfcb470c8904280cd15
icici-xpress <milind@cateina.com>
2020-04-04 19:44:25 +0530

    googleAuth added in auth.js

--- config/auth.js
+++ config/auth.js
@@ -6,6 +6,10 @@ module.exports = {
     'facebookAuth' : {
         'clientID' 		: '149703079756185', // your App ID
         'clientSecret' 	: '923ee32ea26fd46c249d8e300eb7121b', // your App Secret
+    },
+    'googleAuth' : {
+        'clientID' 		: '558266834723-ku2ag4dmt032k2av402sg9hc3kau9frp.apps.googleusercontent.com',
+        'clientSecret' 	: 'crS_zejVrMGCYCPO8uEY9QvP'
     }
 
 

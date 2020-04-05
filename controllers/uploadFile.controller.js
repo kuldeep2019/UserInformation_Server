e3ff5f64c995c5b465795ffc7e1e92057048d53a
icici-xpress <milind@cateina.com>
2020-04-05 20:19:45 +0530

    multer module added upload controller created aadhar folder created with file in it for testingand app.js path for upload controller added

---
+++ controllers/uploadFile.controller.js
@@ -0,0 +1,30 @@
+require('rootpath')();
+var express = require('express');
+var router = express.Router();
+var multer = require('multer');
+
+var storage = multer.diskStorage({
+    // destination
+    destination: function (req, file, cb) {
+        var dir = './aadharFiles/';
+                mkdirp(dir, function (err) {
+                    if (err) {
+                        console.error(err);
+                    }
+                    // move cb to here
+                    cb(null, dir);
+                    //cb(null, os.tmpdir());
+                });
+            },
+            filename: function (req, file, cb) {
+                cb(null, file.originalname);            
+            }
+  });
+
+var upload = multer({ storage: storage})
+
+router.post('/upload', upload.array("file"), fileUpload);
+
+function fileUpload(req, res) {
+    res.status(200).send({data:"File Uploaded"});
+}

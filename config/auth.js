202e31bd27717b223c2ed16a1a96b781181a0e39
icici-xpress <milind@cateina.com>
2020-04-05 13:18:12 +0530

    auth.js clientId and token changed

--- config/auth.js
+++ config/auth.js
@@ -4,13 +4,8 @@
 module.exports = {
 	
     'facebookAuth' : {
-<<<<<<< HEAD
         'clientID' 		: '163580304822250', // App ID
         'clientSecret' 	: 'a3cfef322c0f112f041a8caafc15a9aa', // App Secret,
-=======
-        'clientID' 		: '149703079756185', // App ID
-        'clientSecret' 	: '923ee32ea26fd46c249d8e300eb7121b', // App Secret,
->>>>>>> 6080600943be0d8fd42b9e1e83d5de6e2d9129f7
         'callbackURL'   : "https://localhost:3000/auth/facebook/callback"
 
     },
